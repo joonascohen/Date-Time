6ef2ad9750511631977f0ec7b0628745064bfee4
Joonas Cohen <joonas8cohen@gmail.com>
2022-03-15 10:44:32 -0700

    updated main.js with the time program

--- main.js
+++ main.js
@@ -1,4 +1,110 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+
+]
+const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Sunday'
+
+]
 const pEL = document.getElementById('p-el')
 const myDate = new Date();
-console.log(myDate)
+const formattedDate = formatDate(myDate);
+
+function formatDate(myDate) {
+    const date = {
+        date: myDate.getDate(),
+        fullDay: myDate.getDay(),
+        fullMonth: myDate.getMonth() + 1,
+        year: myDate.getFullYear(),
+        hour: (myDate.getHours() % 12) || 12,
+        minute: myDate.getMinutes(),
+        second: myDate.getSeconds(),
+        amOrPm: myDate.getHours() < 12 ? "AM" : "PM",
+    }
+    switch (date.fullDay) {
+        case 1:
+            date.fullDay = weekdays[0];
+            break;
+        case 2:
+            date.fullDay = weekdays[1];
+            break;
+        case 3:
+            date.fullDay = weekdays[2];
+            break;
+        case 4:
+            date.fullDay = weekdays[3];
+            break;
+        case 5:
+            date.fullDay = weekdays[4];
+            break;
+        case 6:
+            date.fullDay = weekdays[5];
+            break;
+        case 7:
+            date.fullDay = weekdays[6];
+            break;
+    }
+    switch (date.fullMonth) {
+        case 1:
+            date.month = months[0];
+            break;
+        case 2:
+            date.month = months[1];
+            break;
+        case 3:
+            date.month = months[2];
+            break;
+        case 4:
+            date.month = months[3];
+            break;
+        case 5:
+            date.month = months[4];
+            break;
+        case 6:
+            date.month = months[5];
+            break;
+        case 7:
+            date.month = months[6];
+            break;
+        case 8:
+            date.month = months[7];
+            break;
+        case 9:
+            date.month = months[8];
+            break;
+        case 10:
+            date.month = months[9];
+            break;
+        case 11:
+            date.month = months[10];
+            break;
+        case 12:
+            date.month = months[11];
+            break;
+    }
+    console.log(date);
+
+
+    return `${date.fullMonth}/${date.date}/${date.year}
+${date.hour}:${date.minute}${date.amOrPm}
+${date.fullDay}, ${date.month} ${date.date}
+`
+}
 pEL.textContent = myDate;
+console.log(formattedDate)
